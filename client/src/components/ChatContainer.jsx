f135841e699c8c49ee55973f454a8712d6257e8f
rey4799 <reyhanputram4799@gmail.com>
2024-05-21 17:00:16 +0700

    testing

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -3,74 +3,27 @@ import socketIOClient from 'socket.io-client'
 import ChatBoxReciever, { ChatBoxSender } from './ChatBox.jsx'
 import InputText from './InputText'
 import UserLogin from './UserLogin.jsx'
-import {
-    doc,
-    setDoc,
-    collection,
-    serverTimestamp,
-    query,
-    onSnapshot,
-    orderBy,
-} from 'firebase/firestore'
-import db from './firebaseConfig/firebaseConfig.js'
+
 
 export default function ChatContainer() {
-    let socketio = socketIOClient('http://localhost:5001')
+    let socketio = socketIOClient('http://localhost:5000')
     const [chats, setChats] = useState([])
     const [user, setUser] = useState(localStorage.getItem('user'))
     const avatar = localStorage.getItem('avatar')
-    const chatsRef = collection(db, 'Messages')
-    const messagesEndRef = useRef(null)
-    const scrollToBottom = () => {
-        messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' })
-    }
+    
 
     useEffect(() => {
-        scrollToBottom()
-    }, [chats])
-
-    useEffect(() => {
-        socketio.on('chat', (senderChats) => {
+        socketio.on('chat', senderChats => {
             setChats(senderChats)
         })
     })
-
-    useEffect(() => {
-        const q = query(chatsRef, orderBy('createdAt', 'asc'))
-
-        const unsub = onSnapshot(q, (querySnapshot) => {
-            const fireChats = []
-            querySnapshot.forEach((doc) => {
-                fireChats.push(doc.data())
-            })
-            setChats([...fireChats])
-        })
-        return () => {
-            unsub()
-        }
-    }, [])
-
-    function addToFirrebase(chat) {
-        const newChat = {
-            avatar,
-            createdAt: serverTimestamp(),
-            user,
-            message: chat.message,
-        }
-
-        const chatRef = doc(chatsRef)
-        setDoc(chatRef, newChat)
-            .then(() => console.log('Chat added succesfully'))
-            .catch(console.log)
-    }
 
     function sendChatToSocket(chat) {
         socketio.emit('chat', chat)
     }
 
     function addMessage(chat) {
-        const newChat = { ...chat, user: localStorage.getItem('user'), avatar }
-        addToFirrebase(chat)
+        const newChat = { ...chat, user, avatar }
         setChats([...chats, newChat])
         sendChatToSocket([...chats, newChat])
     }
@@ -126,13 +79,6 @@ export default function ChatContainer() {
                         }}
                     >
                         <h4>Username: {user}</h4>
-                        <strong>
-                            Remember to Subscribe to{' '}
-                            <a href="https://www.youtube.com/channel/UCmoQtgmJ2SHEAPCAR1Q8TBA">
-                                {' '}
-                                My Channel
-                            </a>
-                        </strong>
                         <p
                             onClick={() => logout()}
                             style={{ color: 'blue', cursor: 'pointer' }}
@@ -148,24 +94,7 @@ export default function ChatContainer() {
                 <UserLogin setUser={setUser} />
             )}
 
-            <div
-                style={{
-                    margin: 10,
-                    display: 'flex',
-                    justifyContent: 'center',
-                }}
-            >
-                <small
-                    style={{
-                        backgroundColor: 'lightblue',
-                        padding: 5,
-                        borderRadius: 5,
-                    }}
-                >
-                    Interested in some 1 on 1 Coding Tutorials and Mentorship.
-                    Lets chat on Discord: <strong> kutlo_sek#5370 </strong>
-                </small>
-            </div>
+            
         </div>
     )
 }
