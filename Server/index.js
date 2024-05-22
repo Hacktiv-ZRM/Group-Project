31b36ca0122cfbf76d3a3e8544c3cbe61ef156a7
rey4799 <reyhanputram4799@gmail.com>
2024-05-22 11:35:58 +0700

    add socket inside index.js

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,12 @@
 const cors = require("cors");
 const mongoose = require("mongoose");
 const express = require("express");
+const { Server } = require("socket.io");
+require("dotenv").config();
 
 const chatRoute = require("./Routes/chatRoute");
 const messageRoute = require("./Routes/messageRoute");
 const userRoute = require("./Routes/userRoute");
-
-require("dotenv").config();
 
 const app = express();
 
@@ -24,11 +24,61 @@ app.get("/", (req, res) => {
 const uri = process.env.ATLAS_URI;
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server running on port: ${port}...`);
-});
-
 mongoose
   .connect(uri)
   .then(() => console.log("MongoDB connection established..."))
   .catch((error) => console.error("MongoDB connection failed:", error.message));
+
+const server = app.listen(port, () => {
+  console.log(`Server running on port: ${port}...`);
+});
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+let onlineUsers = [];
+
+io.on("connection", (socket) => {
+  console.log('New user connected');
+
+  socket.on("addNewUser", (userId) => {
+    if (!onlineUsers.some((user) => user.userId === userId)) {
+      onlineUsers.push({
+        userId,
+        socketId: socket.id,
+      });
+    }
+
+    console.log("Connected Users:", onlineUsers);
+
+    // send active users
+    io.emit("getUsers", onlineUsers);
+  });
+
+  socket.on("sendMessage", (message) => {
+    const user = onlineUsers.find(
+      (user) => user.userId === message.recipientId
+    );
+
+    if (user) {
+      console.log("sending message and notification");
+      io.to(user.socketId).emit("getMessage", message);
+      io.to(user.socketId).emit("getNotification", {
+        senderId: message.senderId,
+        isRead: false,
+        date: new Date(),
+      });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+    console.log("User Disconnected:", onlineUsers);
+
+    // send active users
+    io.emit("getUsers", onlineUsers);
+  });
+});
